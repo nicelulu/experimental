cdc8e3e392bc9167b2e755d6999056765aa97808
zhangxiao871 <zhangxiao871@jd.com>
2023-04-24 10:26:37 +0800

    test filter

--- FilterTest/test1.cpp
+++ FilterTest/test1.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstring>
 #include <emmintrin.h>
+#include <immintrin.h>
 #include <iomanip>
 #include <iostream>
 #include <random>
@@ -44,27 +45,29 @@ size_t filterSSE(T * res, const uint8_t * filter, size_t size, const T * data)
     {
         uint16_t mask = _mm_movemask_epi8(_mm_cmpeq_epi8(_mm_loadu_si128(reinterpret_cast<const __m128i *>(filt_pos)), zero16));
         mask = ~mask;
-
+//        std::cout << mask << std::endl;
         if (0 == mask)
         {
         }
         else if (0xFFFF == mask)
         {
-            memcpy(res, data_pos, 16 * sizeof(T));
-            res += 16;
+            memcpy(res, data_pos, 16);
+            res += SIMD_BYTES/sizeof(T);
         }
         else
         {
             for (size_t i = 0; i < SIMD_BYTES; ++i)
+            {
                 if (filt_pos[i])
                 {
                     *res = data_pos[i];
                     res++;
                 }
+            }
         }
 
         filt_pos += SIMD_BYTES;
-        data_pos += SIMD_BYTES;
+        data_pos += SIMD_BYTES/sizeof(T);
     }
     return res - begin_res;
 }
@@ -84,47 +87,6 @@ uint64_t gen_random1(size_t range) noexcept {
     static std::uniform_real_distribution<double> dis(0, 1);
     return std::ceil(dis(gen) * range);
 }
-
-//int main(int argc, char ** argv) {
-//    if (argc < 3)
-//    {
-//        std::cout << "error" << std::endl;
-//        return 0;
-//    }
-//
-//    int use_sse = atoi(argv[1]);
-//    size_t size = atoi(argv[2]);
-//
-//    uint32_t * res = new uint32_t[size];
-//    uint8_t * filt = new uint8_t[size];
-//    uint32_t * data = new uint32_t[size];
-//    for (size_t i = 0; i < size; ++i)
-//    {
-//        if (i % 32 < 16)
-//            filt[i] = 1;
-//        else
-//            filt[i] = gen_random() % 2;
-//        data[i] = gen_random();
-//    }
-//
-//    const auto start = std::chrono::high_resolution_clock::now();
-//    size_t res_size;
-//    if (use_sse)
-//        res_size = filterSSE(res, filt, size, data);
-//    else
-//        res_size = filter(res, filt, size, data);
-//    const std::chrono::duration<double> diff =
-//            std::chrono::high_resolution_clock::now() - start;
-//    std::cout << "Time: " << std::fixed << std::setprecision(6) << diff.count()
-//              << " sec " << std::endl;
-//
-//    std::cout << "res_size: " << res_size << std::endl;
-//
-//    delete[] res;
-//    delete[] filt;
-//    delete[] data;
-//
-//}
 
 typedef struct Row
 {
@@ -147,82 +109,173 @@ Row target3_end;
 void initTarget()
 {
     target1.a[0] = 1000;
-    target1.a[1] = 10;
+    target1.a[1] = 9;
 
     target1_end.a[0] = 1000;
-    target1_end.a[1] = 50;
+    target1_end.a[1] = 49;
 
     target2.a[0] = 2000;
-    target2.a[1] = 10;
+    target2.a[1] = 9;
 
     target2_end.a[0] = 2000;
-    target2_end.a[1] = 50;
+    target2_end.a[1] = 49;
 
     target3.a[0] = 3000;
-    target3.a[1] = 10;
+    target3.a[1] = 9;
 
     target3_end.a[0] = 3000;
-    target3_end.a[1] = 50;
+    target3_end.a[1] = 49;
 }
 
 
 
-uint8_t * test1(uint8_t * filter, const Row *rows, int nrows)
+void genFilter(uint8_t * filter, const Row *rows, int nrows)
 {
     size_t size = nrows;
     size_t i = 0;
     while (i < size)
     {
-        if ((rows[i].c >= target1.c && rows[i].c < target1_end.c) || (rows[i].c >= target2.c && rows[i].c < target2_end.c) || (rows[i].c >= target3.c && rows[i].c < target3_end.c))
+        if ((rows[i].c > target1.c && rows[i].c <= target1_end.c) || (rows[i].c > target2.c && rows[i].c <= target2_end.c) || (rows[i].c > target3.c && rows[i].c <= target3_end.c))
             filter[i] = 1;
         else
             filter[i] = 0;
         ++i;
     }
-
-    return filter;
 }
 
-size_t test11(Row * res, const Row *rows, int nrows)
+size_t testDirectly(Row * res, const Row *rows, int nrows)
 {
+    Row * res_pos = res;
     size_t size = nrows;
     size_t i = 0;
     while (i < size)
     {
-        if ((rows[i].c >= target1.c && rows[i].c < target1_end.c) || (rows[i].c >= target2.c && rows[i].c < target2_end.c) || (rows[i].c >= target3.c && rows[i].c < target3_end.c))
-            res[i] = rows[i];
-
+        if ((rows[i].c > target1.c && rows[i].c <= target1_end.c) || (rows[i].c > target2.c && rows[i].c <= target2_end.c) || (rows[i].c > target3.c && rows[i].c <= target3_end.c))
+        {
+            *(res_pos) = rows[i];
+            res_pos++;
+        }
         ++i;
     }
 
-    return i;
+    return res_pos - res;
+}
+
+void print(__m256i var)
+{
+    auto * val = reinterpret_cast<uint64_t *>(&var);
+
+    std::cout << "print _mm256i: " << std::hex;
+    for (size_t i = 0; i < 4; ++i)
+    {
+        std::cout << "" << val[i] << " ";
+    }
+
+    std::cout << std::dec << std::endl;
+}
+
+size_t testAVX(Row * res, const Row * rows, int nrows)
+{
+    Row * res_pos = res;
+    size_t size = nrows;
+    size_t i = 0;
+    for (; i + 3 < size; i += 4)
+    {
+        const __m256i ones = _mm256_set1_epi64x(-1);
+//        print(ones);
+        const __m256i data = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(rows + i));
+//        print(data);
+        const __m256i range1_l = _mm256_set1_epi64x(target1.c);
+//        print(range1_l);
+        const __m256i range1_r = _mm256_set1_epi64x(target1_end.c);
+//        print(range1_r);
+        const __m256i range2_l = _mm256_set1_epi64x(target2.c);
+//        print(range2_l);
+        const __m256i range2_r = _mm256_set1_epi64x(target2_end.c);
+//        print(range2_r);
+        const __m256i range3_l = _mm256_set1_epi64x(target3.c);
+//        print(range3_l);
+        const __m256i range3_r = _mm256_set1_epi64x(target3_end.c);
+//        print(range3_r);
+
+        const __m256i range1_l_res = _mm256_cmpgt_epi64(data, range1_l);
+//        print(range1_l_res);
+        const __m256i range1_r_res = _mm256_xor_si256(_mm256_cmpgt_epi64(data, range1_r), ones);
+//        print(range1_r_res);
+        const __m256i range1_res = _mm256_and_si256(range1_l_res, range1_r_res);
+//        print(range1_res);
+
+        const __m256i range2_l_res = _mm256_cmpgt_epi64(data, range2_l);
+//        print(range2_l_res);
+        const __m256i range2_r_res = _mm256_xor_si256(_mm256_cmpgt_epi64(data, range2_r), ones);
+//        print(range2_r_res);
+        const __m256i range2_res = _mm256_and_si256(range2_l_res, range2_r_res);
+//        print(range2_res);
+
+        const __m256i range3_l_res = _mm256_cmpgt_epi64(data, range3_l);
+//        print(range3_l_res);
+        const __m256i range3_r_res = _mm256_xor_si256(_mm256_cmpgt_epi64(data, range3_r), ones);
+//        print(range3_r_res);
+        const __m256i range3_res = _mm256_and_si256(range3_l_res, range3_r_res);
+//        print(range3_res);
+
+        const __m256i range_res = _mm256_or_si256(range1_res, _mm256_or_si256(range2_res, range3_res));
+//        print(range_res);
+
+        uint32_t range_res_mask = _mm256_movemask_epi8(range_res);
+//        std::cout << "range_res_mask: " << range_res_mask << std::endl;
+        if (0 == range_res_mask)
+        {
+
+        }
+        else if (0xFFFFFFFF == range_res_mask)
+        {
+            memcpy(res_pos, rows + i, 4 * sizeof(Row));
+            res_pos += 4;
+        }
+        else
+        {
+            for (size_t j = 0; j < 4; ++j)
+            {
+                auto * filter = reinterpret_cast<uint8_t *>(&range_res_mask);
+                if (filter[j])
+                {
+                    *(res_pos) = *(rows + i + j);
+                    res_pos++;
+                }
+            }
+        }
+    }
+
+    for (; i < size; i++)
+    {
+        if ((rows[i].c > target1.c && rows[i].c <= target1_end.c) || (rows[i].c > target2.c && rows[i].c <= target2_end.c) || (rows[i].c > target3.c && rows[i].c <= target3_end.c))
+        {
+            *(res_pos) = rows[i];
+            res_pos++;
+        }
+    }
+
+    return res_pos - res;
 }
 
 int main(int argc, char ** argv) {
-    if (argc < 4)
+    if (argc < 3)
     {
-        std::cout << "param size error" << std::endl;
+        std::cout << "param size error. First param is data size, second param is target data ratio." << std::endl;
         return 0;
     }
     initTarget();
 
-    int use_sse = atoi(argv[1]);
-    size_t size = atoi(argv[2]);
-    size_t target_data_ratio = atoi(argv[3]);
+    size_t size = atoi(argv[1]);
+    size_t target_data_ratio = atoi(argv[2]);
 
-    std::cout << gen_random1(target_data_ratio) << "------------" << gen_random1(target_data_ratio) << std::endl;
-    std::cout << gen_random1(target_data_ratio) << "------------" << gen_random1(target_data_ratio) << std::endl;
-    std::cout << gen_random1(target_data_ratio) << "------------" << gen_random1(target_data_ratio) << std::endl;
-    std::cout << gen_random() << "------------" << gen_random() << std::endl;
-    std::cout << gen_random() << "------------" << gen_random() << std::endl;
-    std::cout << gen_random() << "------------" << gen_random() << std::endl;
-
-    Row * data = new Row[size];
+    std::vector<Row> data(size);
     for (size_t i = 0; i < size; ++i)
     {
         if (gen_random1(target_data_ratio) == 1)
         {
-            data[i].c = target1.c;
+            data[i].c = target1_end.c;
         }
         else
         {
@@ -230,34 +283,60 @@ int main(int argc, char ** argv) {
         }
     }
 
-    auto * filter = new uint8_t[size];
-    Row * res = new Row[size];
-    size_t res_size;
-    const auto start = std::chrono::high_resolution_clock::now();
-    if (use_sse == 2)
+    /// testDirectly
     {
-        res_size = test11(res, data, size);
-    }
-    else
-    {
-        filter = test1(filter, data, size);
+        std::vector<Row> res(size);
 
-        if (use_sse)
-            res_size = filterSSE(res, filter, size, data);
-        else
-            res_size = filterMethod(res, filter, size, data);
+        const auto start = std::chrono::high_resolution_clock::now();
+        size_t res_size = testDirectly(&res[0], &data[0], size);
+
+        const std::chrono::duration<double> diff =
+                std::chrono::high_resolution_clock::now() - start;
+        std::cout << "testDirectly Time: " << std::fixed << std::setprecision(6) << diff.count()
+                  << " sec " << "res_size: " << res_size << std::endl;
     }
 
-    const std::chrono::duration<double> diff =
-            std::chrono::high_resolution_clock::now() - start;
-    std::cout << "Time: " << std::fixed << std::setprecision(6) << diff.count()
-              << " sec " << std::endl;
+    /// testAVX
+    {
+        std::vector<Row> res(size);
 
-    std::cout << "res_size: " << res_size << std::endl;
+        const auto start = std::chrono::high_resolution_clock::now();
+        size_t res_size = testAVX(&res[0], &data[0], size);
 
-    delete[] res;
-    delete[] filter;
-    delete[] data;
+        const std::chrono::duration<double> diff =
+                std::chrono::high_resolution_clock::now() - start;
+        std::cout << "testAVX Time: " << std::fixed << std::setprecision(6) << diff.count()
+                  << " sec " << "res_size: " << res_size << std::endl;
+    }
+
+    /// filterSSE
+    {
+        std::vector<Row> res(size);
+        std::vector<uint8_t> filter(size);
+
+        const auto start = std::chrono::high_resolution_clock::now();
+        genFilter(&filter[0], &data[0], size);
+        size_t res_size = filterSSE(&res[0], &filter[0], size, &data[0]);
+
+        const std::chrono::duration<double> diff =
+                std::chrono::high_resolution_clock::now() - start;
+        std::cout << "filterSSE Time: " << std::fixed << std::setprecision(6) << diff.count()
+                  << " sec " << "res_size: " << res_size << std::endl;
+    }
+
+    /// filterMethod
+    {
+        std::vector<Row> res(size);
+        std::vector<uint8_t> filter(size);
+
+        const auto start = std::chrono::high_resolution_clock::now();
+        genFilter(&filter[0], &data[0], size);
+        size_t res_size = filterMethod(&res[0], &filter[0], size, &data[0]);
+
+        const std::chrono::duration<double> diff =
+                std::chrono::high_resolution_clock::now() - start;
+        std::cout << "filterMethod Time: " << std::fixed << std::setprecision(6) << diff.count()
+                  << " sec " << "res_size: " << res_size << std::endl;
+    }
 
 }
-
--- InvalidLock/invalid_lock.cpp
+++ InvalidLock/invalid_lock.cpp
@@ -1,62 +1,62 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-
-void perpos(const string & s, int left, int right, vector<vector<bool>> & dp)
-{
-    while(left >=0 && right < s.size() && s[left] == s[right])
-    {
-        dp[left][right] = true;
-        left--;
-        right++;
-    }
-    return;
-}
-
-void dfs(vector<vector<string>> & ans,vector<string> & path, const string & s, int ind, vector<vector<bool>>& dp)
-{
-    if(ind == s.size())
-    {
-        ans.push_back(path);
-        return;
-    }
-    for(int i = ind; i < s.size(); i++)
-    {
-        if(!dp[ind][i])
-            continue;
-        path.push_back(s.substr(ind,i+1-ind));
-        dfs(ans,path,s,i+1,dp);
-        path.pop_back();
-    }
-    return;
-}
-
-vector<vector<string>> test(const string & s)
-{
-    vector<vector<string>> ans;
-    int len = s.size();
-    vector<vector<bool>> dp(len,vector<bool>(len,false));
-    for(int i = 0; i < len; i++)
-    {
-        perpos(s,i,i,dp);
-        perpos(s,i,i+1,dp);
-    }
-    vector<string> path;
-    dfs(ans,path,s,0,dp);
-    return ans;
-}
-
-int main()
-{
-    string s("aab");
-    vector<vector<string>> res = test(s);
-    for (size_t i = 0; i < res.size(); ++i)
-    {
-        for (size_t j = 0; j < res[i].size(); ++j)
-        {
-            std::cout << res[i][j] << std::endl;
-        }
-    }
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//
+//void perpos(const string & s, int left, int right, vector<vector<bool>> & dp)
+//{
+//    while(left >=0 && right < s.size() && s[left] == s[right])
+//    {
+//        dp[left][right] = true;
+//        left--;
+//        right++;
+//    }
+//    return;
+//}
+//
+//void dfs(vector<vector<string>> & ans,vector<string> & path, const string & s, int ind, vector<vector<bool>>& dp)
+//{
+//    if(ind == s.size())
+//    {
+//        ans.push_back(path);
+//        return;
+//    }
+//    for(int i = ind; i < s.size(); i++)
+//    {
+//        if(!dp[ind][i])
+//            continue;
+//        path.push_back(s.substr(ind,i+1-ind));
+//        dfs(ans,path,s,i+1,dp);
+//        path.pop_back();
+//    }
+//    return;
+//}
+//
+//vector<vector<string>> test(const string & s)
+//{
+//    vector<vector<string>> ans;
+//    int len = s.size();
+//    vector<vector<bool>> dp(len,vector<bool>(len,false));
+//    for(int i = 0; i < len; i++)
+//    {
+//        perpos(s,i,i,dp);
+//        perpos(s,i,i+1,dp);
+//    }
+//    vector<string> path;
+//    dfs(ans,path,s,0,dp);
+//    return ans;
+//}
+//
+//int main()
+//{
+//    string s("aab");
+//    vector<vector<string>> res = test(s);
+//    for (size_t i = 0; i < res.size(); ++i)
+//    {
+//        for (size_t j = 0; j < res[i].size(); ++j)
+//        {
+//            std::cout << res[i][j] << std::endl;
+//        }
+//    }
+//}
